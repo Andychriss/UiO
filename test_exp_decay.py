a891f7ef90815c5eaa32700af0cfcb0581f8670c
Jonathan Larsen <jonathanel99@gmail.com>
2020-09-20 14:04:50 +0200

    Lagde exp_decay.py og test_exp_decay.py er ikke helt sikker på at det er riktig men plottene og testen ser bra ut

---
+++ test_exp_decay.py
@@ -0,0 +1,8 @@
+import exp_decay
+import pytest
+
+def test_ExpontentialDecay():
+    ODE = exp_decay.ExponetialDecay(0.4)
+    ODEder = ODE(1, 3.2)
+    tol = 10e-7
+    assert abs(ODEder - (-1.28)) < tol
