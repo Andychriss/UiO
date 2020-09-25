651ef9997b84d2fe76bc530524627b7f1ab6d670
Jonathan Larsen <jonathanel99@gmail.com>
2020-09-25 13:35:33 +0200

    La til properties decorators og endret litt på __init__ slik at den bare tar L og M som argumenter

--- pendulum.py
+++ pendulum.py
@@ -6,8 +6,7 @@ from scipy.integrate import solve_ivp
 g = 9.81
 
 class Pendulum():
-    def __init__(self, theta, L = 1, M = 1):
-        self.theta = theta
+    def __init__(self, L = 1, M = 1):
         self.L = L
         self.M = M
         
@@ -19,7 +18,7 @@ class Pendulum():
         return f
 
     
-    def solve(self, y0, T, dt, angles):
+    def solve(self, y0, T, dt, angles = "rad"):
         self.angles = angles
         if angles == 'deg':
             y0 = y0*(pi/180)
@@ -28,23 +27,30 @@ class Pendulum():
         time = T/dt
         time = np.linspace(0, T, int(time))
         sol = solve_ivp(self.__call__, t, y0, t_eval=time)
-        sol.t = np.array(sol.t)
-        sol.y = np.array(sol.y)
+        self.solution_t = np.array(sol.t)
+        self.solution_theta = np.array(sol.y[0])
+        self.solution_omega = np.array(sol.y[1])
 
     @property
-    def t(i, dt)
-        t = i * dt
-        t = np.array(t)
+    def t(self):
+        return self.solution_t
     
     @property
-    def theta(theta, t)
-        theta = theta*t
-        theta = np.array(theta)
+    def theta(self):
+        return self.solution_theta
     
     @property
-    def omega(omega, t)
-        omega = omega*t
-        omega = np.array(omega)
+    def omega(self):
+        return self.solution_omega
 
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
 
-    
+    ODE = Pendulum(2.7, 1)
+    ODE.solve([1, 1], 10, 0.1)
+    print(ODE.t)
+    print(ODE.theta)
+    print(ODE.omega)
+    plt.plot(ODE.t, ODE.theta, color = "Green")
+    plt.plot(ODE.t, ODE.omega, color = "Red")
+    plt.show()
--- test_pendulum.py
+++ test_pendulum.py
@@ -8,7 +8,7 @@ from numpy import pi
     "x", [(-109/60, 0.15)]
 )
 def test_pendulum(x):
-    ODE = pendulum.Pendulum(pi/6, 2.7, 1)
+    ODE = pendulum.Pendulum(2.7, 1)
     ODEder = ODE(0, [0.15, pi/6])
     tol = 10e-7
     assert abs(ODEder[0] - x[0]) < tol
